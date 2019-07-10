952f9f69a0c24858fe72036617a37e36d81f28da
Mikita Kryvitski <solairerove@gmail.com>
2019-07-10 20:15:55 +0300

    feature: just move to package

---
+++ functions/basic.go
@@ -0,0 +1,36 @@
+package functions
+
+import "fmt"
+
+// Basic tbd
+func Basic() {
+	foo()
+	bar("James")
+
+	w := woo("James")
+	fmt.Println(w)
+
+	s, b := gulag("gulag", "mikita")
+	fmt.Println(s)
+	fmt.Println(b)
+}
+
+func foo() {
+	fmt.Println("hello from foo")
+}
+
+// pass by VALUE
+func bar(s string) {
+	fmt.Printf("hello from bar to %v\n", s)
+}
+
+func woo(s string) string {
+	return fmt.Sprintf("hello from woo to %v\t", s)
+}
+
+func gulag(s1, s2 string) (string, bool) {
+	s := fmt.Sprintf("hello from %v to %v\t", s1, s2)
+	b := false
+
+	return s, b
+}
--- main.go
+++ main.go
@@ -2,38 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	basic "github.com/solairerove/go-functions-playground/functions"
 )
 
 func main() {
 	fmt.Println("go-functions-playground")
 
-	foo()
-	bar("James")
-
-	w := woo("James")
-	fmt.Println(w)
-
-	s, b := gulag("gulag", "mikita")
-	fmt.Println(s)
-	fmt.Println(b)
-}
-
-func foo() {
-	fmt.Println("hello from foo")
-}
-
-// pass by VALUE
-func bar(s string) {
-	fmt.Printf("hello from bar to %v\n", s)
-}
-
-func woo(s string) string {
-	return fmt.Sprintf("hello from woo to %v\t", s)
-}
-
-func gulag(s1, s2 string) (string, bool) {
-	s := fmt.Sprintf("hello from %v to %v\t", s1, s2)
-	b := false
-
-	return s, b
+	basic.Basic()
 }
